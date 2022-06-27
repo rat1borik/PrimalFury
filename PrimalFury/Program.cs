83c7f208eaa80129070d223ea5bf51cb446785e8
ga4ik <ga4ik@mail.ru>
2022-06-28 00:51:26 +0300

    feat: crosshair, basic architecture model, debuginfo for mouse

--- PrimalFury/Program.cs
+++ PrimalFury/Program.cs
@@ -23,7 +23,13 @@ namespace PrimalFury
         // Debug info
         static string PreviousKey = "";
         static int PreviousKeyCount = 0;
+        static string MouseKeyPressed = "";
         static string debugText="";
+
+        //HUD Parameters
+        static Sprite crosshair;
+        static uint crosshairRectX=25, crosshairRectY = 25;
+        static string fileCrosshair= "crosshair.png";
         static void Main(string[] args)
         {
             // get Desktop params
@@ -33,11 +39,21 @@ namespace PrimalFury
             window=new RenderWindow(new SFML.Window.VideoMode(WindowWidth,WindowHeight), "PrimalFury", Styles.Fullscreen);
 
             window.SetVerticalSyncEnabled(true);
-            //window.SetMouseCursorVisible(false);
+            window.SetMouseCursorVisible(false);
 
             window.KeyPressed += Window_KeyPressed;
             window.Closed += Window_Closed;
             window.MouseMoved += Window_MouseMoved;
+            window.MouseButtonPressed += Window_MouseButtonPressed;
+
+            Vector2u crosshairRect=new Vector2u(crosshairRectX, crosshairRectY);
+            Image crosshairImg = new Image(fileCrosshair);
+            // Load all resources
+            crosshair = new Sprite() {
+                Texture = new Texture(fileCrosshair),
+                Position = new Vector2f((int)WindowWidth / 2 - crosshairRect.X / 2, (int)WindowHeight / 2 - crosshairRect.Y / 2),
+                Scale = new Vector2f((float)crosshairRect.X/ (float)crosshairImg.Size.X , (float)crosshairRect.Y / (float)crosshairImg.Size.Y)
+            };
 
             var circle = new CircleShape(500f, 100)
             {
@@ -55,14 +71,17 @@ namespace PrimalFury
             while (window.IsOpen)
             {
                 //debug
-                debugText= String.Format("X: {0}, Y: {1}\nKey pressed: {2} : {3} times", MouseX, MouseY, PreviousKey, PreviousKeyCount);
+                debugText= String.Format("X: {0}, Y: {1}\nKey pressed: {2} : {3} times\nMouse key pressed: {4}", MouseX, MouseY, PreviousKey, PreviousKeyCount, MouseKeyPressed);
 
 
                 debugInfo.DisplayedString = debugText;
                 debugInfo.Position = new Vector2f(WindowWidth-(debugText.Split('\n').OrderByDescending(s=>s.Length).ToArray()[0].Length*(debugInfo.CharacterSize/2)) - 20, 0);
                 //circle.Position= new Vector2f(MouseX,MouseY);
                 circle.Position = new Vector2f(circle.Position.X+ (int)Math.Round(kMouse * XDiff), circle.Position.Y + (int)Math.Round(kMouse*YDiff));
+
+
                 window.Clear();
+                //window.Clear(Color.White);
                 // Process events
                 window.DispatchEvents();
 
@@ -73,6 +92,7 @@ namespace PrimalFury
                 #endif
                 window.Draw(circle);
 
+                RenderHUD();
                 // Finally, display the rendered frame on screen
                 window.Display();
             }
@@ -80,13 +100,20 @@ namespace PrimalFury
 
         }
 
+        private static void Window_MouseButtonPressed(object sender, MouseButtonEventArgs e) {
+            MouseKeyPressed = e.Button.ToString();
+        }
+
         private static void Window_MouseMoved(object sender, MouseMoveEventArgs e) {
             XDiff = (int)(e.X - WindowWidth / 2);
             YDiff = (int)(e.Y - WindowHeight / 2);
             MouseX = (uint)e.X;
             MouseY = (uint)e.Y;
-            if(((Window)sender).HasFocus())
-            Mouse.SetPosition(new Vector2i((int)WindowWidth / 2, (int)WindowHeight / 2), (Window)sender);
+            if (((Window)sender).HasFocus())
+                Mouse.SetPosition(new Vector2i((int)WindowWidth / 2, (int)WindowHeight / 2), (Window)sender);
+            else {
+                XDiff = 0; YDiff = 0;
+            }
         }
 
         private static void Window_Closed(object sender, EventArgs e)
@@ -107,6 +134,9 @@ namespace PrimalFury
             {
                 window.Close();
             }
+        }
+        private static void RenderHUD() {
+            window.Draw(crosshair);
         }
     }
 }
--- PrimalFury/Viewport.cs
+++ PrimalFury/Viewport.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SFML.Window;
+using SFML.Graphics;
+using SFML.System;
 
 namespace PrimalFury {
+    internal class Viewport {
+        public Viewport(Vector2u screen, Player p) {
+
+        }
+    }
 }
